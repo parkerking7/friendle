437109529be7bb78e727759d3785c467e74ac243
Parker King <parker.king0807@gmail.com>
2022-02-09 19:42:25 -0700

    more boiler plate

---
+++ main.js
@@ -0,0 +1,17 @@
+$('#singleWord').submit(function(event){
+    event.preventDefault();
+    var word = $("#inputSingleWord").val()
+    console.log(word);
+})
+
+// Form Validation, 5 letters, english word (free word library for this I believe)
+
+
+// Separating Word into an array
+
+// Letter Positioning in the array (Green, Red)
+
+// Create/Or unlock new inputs
+
+// Saving Stats Display
+
